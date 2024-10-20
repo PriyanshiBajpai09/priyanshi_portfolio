e6a6975097500fa8b4714d609030d123c3aa60aa
PriyanshiBajpai09 <priyanshi2004bajpai@gmail.com>
2024-10-21 02:33:05 +0530

    Deploy

---
+++ script.js
@@ -0,0 +1,20 @@
+alert("hello wolrd");
+
+console.log("code is running...");
+var a = prompt("enter number")
+var isTrue = confirm("are you sure you want to leave this page")
+
+
+if (isTrue)
+{
+    console.log("computer is shutting")
+}
+
+else{
+    console.log("computer is not shutting")
+}
+
+
+document.title="hey i am good"
+
+document.body.style.backgroundColor = "red" //this code add inline css
